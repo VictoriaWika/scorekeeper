ec1334d74e04e62b746665fdcc84f9f379d37aef
Victoria Wika <victoriawika@icloud.com>
2021-02-23 11:25:25 +0100

    Add GameForm logic

--- src/components/GameForm/GameForm.js
+++ src/components/GameForm/GameForm.js
@@ -2,9 +2,9 @@ import Button from '../Button'
 import Input from '../Input'
 import './GameForm.css'
 
-export default function GameForm() {
+export default function GameForm(onCreateGame) {
   return (
-    <form className="GameForm">
+    <form onSubmit={handleSubmit} className="GameForm">
       <Input labelText="Name of Game:" placeholderText="e.g. Carcassonne" />
       <Input
         labelText="Player names:"
@@ -13,4 +13,16 @@ export default function GameForm() {
       <Button text="Create game" />
     </form>
   )
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const { nameOfGame, playerNames } = form.elements
+
+    onCreateGame({
+      nameOfGame: nameOfGame.value,
+      playerNames: playerNames.value.split(',').map(name => name.trim()),
+    })
+    form.reset()
+    nameOfGame.focus()
+  }
 }
